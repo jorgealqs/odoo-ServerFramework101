1e80f2a409f570a619b6a7765085c7d48e4472a0
judaqui123 <judaqui123@gmail.com>
2024-04-26 10:50:00 -0500

    tutorial-odoo/Chapter2_ANewApplication

---
+++ addons/tutorial/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Jorge Alberto Quiroz Sierra",
+    'category': 'Category',
+    'description': """
+    ODOO Tutorial for documentation and learning better techniques
+    """,
+    'images': ['static/description/icon.png'],
+    # data files always loaded at installation
+    'data': [
+        # 'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        # 'demo/demo_data.xml',
+    ],
+    'license': 'GPL-3'
+}
