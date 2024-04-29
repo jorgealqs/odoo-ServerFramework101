005b1bb48034e71a7db13ee57e9e6de24574ddd8
judaqui123 <judaqui123@gmail.com>
2024-04-29 10:12:51 -0500

    TutorialOdoo/Chapter11_AddTheSprinkles

--- addons/tutorial/models/estate_property_tag.py
+++ addons/tutorial/models/estate_property_tag.py
@@ -3,11 +3,13 @@ from odoo import fields, models
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Real Estate Property Tag"
+    _order = "name"
 
     name = fields.Char(
         string="Name",
         required=True
     )
+    color = fields.Integer(string="Color")
 
     _sql_constraints = [
         (
--- addons/tutorial/models/estate_property_type.py
+++ addons/tutorial/models/estate_property_type.py
@@ -1,13 +1,37 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Real Estate Property Type"
+    _order = "name"
 
     name = fields.Char(
         string="Name",
         required=True
     )
+    property_ids = fields.One2many(
+        comodel_name='estate.property',
+        inverse_name='property_type_id',
+        string='Properties'
+    )
+    sequence = fields.Integer(
+        string='Sequence',
+        default=1
+    )
+    offer_ids = fields.One2many(
+        comodel_name='estate.property.offer',
+        inverse_name='property_type_id',
+        string='Offers'
+    )
+    offer_count = fields.Integer(
+        string='Offers',
+        compute='_compute_offer_count'
+    )
+
+    @api.depends('offer_ids')
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.property_ids.offer_ids)
 
     _sql_constraints = [
         (
