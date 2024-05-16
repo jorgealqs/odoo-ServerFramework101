786ccd55816cffe1915a5b598ab6b114336b7518
judaqui123 <judaqui123@gmail.com>
2024-05-16 16:35:13 -0500

    TutorialOdooClikerGame/6_HumanizeTheDisplayedValue

--- addons/awesome_clicker/static/src/client_action/client_action.js
+++ addons/awesome_clicker/static/src/client_action/client_action.js
@@ -3,11 +3,13 @@
 import { Component, useState } from "@odoo/owl"
 import { registry } from "@web/core/registry"
 import { useService } from "@web/core/utils/hooks"
-import { useClicker } from "../clicker_hook";
+import { useClicker } from "../clicker_hook"
+import { ClickerValue } from "../clicker_value/clicker_value"
 
 class ClickerClientAction extends Component {
     static template = "awesome_clicker.ClickerClientAction"
     static props = ['*']
+    static components = { ClickerValue }
 
     setup() {
         // this.clickService = useState(useService("awesome_clicker.clicker"))
