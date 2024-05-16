1402ca78ce432d4a3aaad34c27d225ffcf4732ac
judaqui123 <judaqui123@gmail.com>
2024-05-16 11:21:01 -0500

    TutorialOdooClikerGame/1_CreateAsystrayItem

---
+++ addons/awesome_clicker/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Awesome Clicker Game",
+
+    'summary': """
+        In this game, the goal is to accumulate a large number of clicks, and to automate the system
+    """,
+
+    'description': """|
+        In this game, the goal is to accumulate a large number of clicks, and to automate the system"
+    """,
+    'sequence':-4,
+
+    'author': "Jorge Alberto Quiroz Sierra",
+    'website': "",
+    'category': 'Tutorials/AwesomeClicker',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base', 'web'],
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'awesome_clicker/static/src/**/*',
+        ],
+
+    },
+    'license': 'AGPL-3'
+}
